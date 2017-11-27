7340113d98c70552829c99d54d5938322397c3f3
舒克_开飞机 <>
2017-11-27 13:56:20 +0800

    test

--- src/main/java/com/semir/workflow/contorller/Test.java
+++ src/main/java/com/semir/workflow/contorller/Test.java
@@ -1,18 +1,4 @@
 package com.semir.workflow.contorller;
 
-import com.semir.workflow.domain.FlowJob;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class Test {
-    //@Autowired
-    //FlowJob flowJob;
-     @RequestMapping
-    public String SayHello(){
-         return   null;
-     }
-
-
 }
