38b2cefc50a5ad4f64e0cfa4d77fa3e8df1ec47b
舒克_开飞机 <>
2017-11-28 11:50:45 +0800

    test

--- src/main/java/com/semir/workflow/contorller/Test.java
+++ src/main/java/com/semir/workflow/contorller/Test.java
@@ -1,5 +1,27 @@
 package com.semir.workflow.contorller;
 
+import com.semir.workflow.domain.Flow_Job;
+import com.semir.workflow.repository.FlowJobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class Test {
-    
+
+    @Autowired
+    private FlowJobRepository flowJobRepository;
+
+    @RequestMapping(value = "/flowjob",method = RequestMethod.GET)
+    public List<Flow_Job> getAllJob(){
+        return flowJobRepository.findAll();
+    }
+
+
+
+
+
 }
